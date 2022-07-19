5771821d308f46893b9500c1973a828d1698f126
Alexlg <Alejandrolg86@gmail.com>
2022-07-19 12:18:25 +0200

    feat: added catch errors

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,26 +15,48 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function(){
-    return['Bienvenido a mi aplicación'];
+Route::get('/', function () {
+    return ['YEEEPA'];
 });
 
-Route::get('/users', function(){
-    return['get'];
+Route::get('/users', function () {
+    try {
+        $users = DB::table('users')
+            ->select('id', 'name', 'email', 'title')
+            ->get()
+            ->toArray();
+
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'users retrieved successfully',
+                'data' => $users
+            ],
+            200
+        );
+    } catch (\Exception $exception) {
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'Error retrieving: '.$exception->getMessage()
+            ],
+            500
+        );
+    }
 });
 
-Route::post('/users', function(){
-    return['post'];
+Route::post('/users', function () {
+    return ['post'];
 });
 
-Route::put('/users', function(){
-    return['update'];
+Route::put('/users', function () {
+    return ['update'];
 });
 
-Route::delete('/users', function(){
-    return['delete'];
+Route::delete('/users', function () {
+    return ['delete'];
 });
 
-Route::get('/users/{id}', function($id){
+Route::get('/users/{id}', function ($id) {
     return $id;
 });
